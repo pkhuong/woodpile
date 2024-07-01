f2116af432aa01c514ed9b33bcdc50d5d6b4f407
Paul Khuong <pvk@pvk.ca>
2024-07-01 04:13:27 +0000

    examples: close better

--- woodpile/examples/reader.rs
+++ woodpile/examples/reader.rs
@@ -4,35 +4,81 @@ use std::io::Result;
 use std::path::Path;
 use std::sync::Arc;
 
-/// n=100000 min=2ms405µs208ns p1=3ms685µs176ns p5=4ms610µs257ns p50=9ms425µs977ns p90=18ms196µs67ns p95=22ms704µs798ns p99=34ms817µs536ns p999=59ms172µs212ns max=100ms237µs166ns
-
-/// FSx v3 n=100016 min=5ms533µs644ns p1=7ms766µs623ns p5=9ms999µs130ns p50=22ms490µs773ns p90=40ms646µs65ns p95=47ms931µs588ns p99=63ms637µs992ns p999=83ms31µs28ns max=115ms230µs910ns
-/// FSx v4 n=100007 min=5ms239µs856ns p1=7ms309µs183ns p5=9ms256µs786ns p50=20ms9µs484ns p90=34ms961µs534ns p95=40ms878µs156ns p99=55ms592µs822ns p999=84ms893µs564ns max=139ms708µs227ns
+// writer: n=100500 min=1ms879µs237ns p1=2ms94µs472ns p5=2ms181µs689ns p50=2ms491µs461ns p90=3ms945µs363ns p95=5ms95µs272ns p99=8ms858µs944ns p999=18ms796µs730ns max=45ms268µs840ns
+// NO RESCAN
+// reader: n=100000 min=2ms109µs61ns p1=2ms400µs360ns p5=2ms546µs958ns p50=5ms289µs322ns p90=9ms866µs65ns p95=13ms95µs154ns p99=23ms627µs114ns p999=45ms699µs664ns max=83ms225µs571ns
+//         n=100000 min=1µs533ns p1=155µs311ns p5=464µs135ns p50=2ms328µs109ns p90=5ms426µs393ns p95=7ms66µs488ns p99=13ms862µs937ns p999=30ms554µs35ns max=80ms912µs729ns
+//          n=85179 min=1ms7µs156ns p1=1ms330µs658ns p5=1ms487µs187ns p50=2ms258µs455ns p90=4ms726µs7ns p95=6ms244µs831ns p99=11ms191µs635ns p999=24ms644µs310ns max=60ms630µs437ns
+// CURRENT
+// reader: n=100000 min=2ms385µs727ns p1=3ms178µs37ns p5=3ms981µs82ns p50=8ms172µs514ns p90=15ms507µs824ns p95=20ms243µs508ns p99=35ms473µs181ns p999=63ms517µs392ns max=111ms153µs97ns
+//         n=100000 min=2µs949ns p1=130µs23ns p5=406µs525ns p50=4ms91µs769ns p90=7ms662µs241ns p95=10ms539µs638ns p99=19ms847µs763ns p999=42ms510µs502ns max=85ms81µs869ns
+//          n=84081 min=1ms644µs606ns p1=2ms440µs760ns p5=2ms833µs865ns p50=3ms951µs581ns p90=7ms408µs296ns p95=9ms257µs987ns p99=16ms211µs224ns p999=35ms7µs833ns max=85ms421µs217ns
 
 fn maintainer(log: woodpile::LogMaintainer) {
+    use std::collections::HashMap;
+    use std::path::PathBuf;
+
+    let mut pending_close: HashMap<PathBuf, time::PrimitiveDateTime> = HashMap::new();
     loop {
-        std::thread::sleep(std::time::Duration::from_millis(500));
+        std::thread::sleep(std::time::Duration::from_millis(1500));
         let _ = vouched_time::nfs_voucher::scan_base_time();
-        let mut num_closed = 0usize;
         for (path, deadline) in log.enumerate_subdirs_lifo() {
             let now = time::OffsetDateTime::now_utc();
             let now = time::PrimitiveDateTime::new(now.date(), now.time());
             if now > deadline {
-                if (now - deadline) >= time::Duration::seconds(60) {
+                if (now - deadline) >= time::Duration::seconds(120) {
                     break;
                 }
 
-                log.close_subdir(
+                if pending_close.contains_key(&path) {
+                    break;
+                }
+
+                let begin = std::time::Instant::now();
+                match log
+                    .close_subdir(
+                        &path,
+                        vouched_time::VouchedTime::now_or_die(
+                            vouched_time::nfs_voucher::get_base_time,
+                        ),
+                        Default::default(),
+                    )
+                    .unwrap()
+                {
+                    None => {} // Done!
+                    Some(deadline) => {
+                        pending_close.insert(path.clone(), deadline);
+                        println!("Initial close {:?} in {:?}", path, begin.elapsed());
+                    }
+                }
+            }
+        }
+
+        let mut closed_paths: Vec<PathBuf> = Vec::new();
+        for (path, deadline) in pending_close.iter() {
+            let now = time::OffsetDateTime::now_utc();
+            let now = time::PrimitiveDateTime::new(now.date(), now.time());
+            if now <= *deadline {
+                continue;
+            }
+
+            let begin = std::time::Instant::now();
+            if log
+                .close_subdir(
                     &path,
                     vouched_time::VouchedTime::now_or_die(vouched_time::nfs_voucher::get_base_time),
                     Default::default(),
                 )
-                .unwrap();
-                num_closed += 1;
-                if num_closed >= 4 {
-                    break;
-                }
+                .unwrap()
+                .is_none()
+            {
+                closed_paths.push(path.clone());
+                println!("Final close {:?} in {:?}", path, begin.elapsed());
             }
+        }
+
+        for path in closed_paths {
+            pending_close.remove(&path);
         }
     }
 }
@@ -68,8 +114,12 @@ fn main() -> Result<()> {
     while observations.len() < 120_000 {
         let begin = std::time::Instant::now();
         let now = vouched_time::VouchedTime::now_or_die(vouched_time::nfs_voucher::get_base_time);
+        let options = woodpile::CacheOptions {
+            mode: woodpile::ReaderMode::ReadOnly,
+            ..Default::default()
+        };
         reader
-            .maintain_cache(now, time::Duration::seconds(10), Default::default())
+            .maintain_cache(now, time::Duration::seconds(10), options)
             .unwrap();
         let end = time::OffsetDateTime::now_utc().unix_timestamp_nanos();
 
